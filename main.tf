d8b46820a46bb7081e39c8acab769c2dd5e18242
Thiago Oliveira <toliveirat@icloud.com>
2024-01-29 15:11:32 +0000

    fix id issues

--- main.tf
+++ main.tf
@@ -38,10 +38,10 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog.blog_alb.target_group_arns
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
-  image_id      = data.aws_ami.id
+  image_id      = data.aws_ami.app_ami
   instance_type = var.instance_type
   }
 
@@ -71,7 +71,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
