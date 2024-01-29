279e748eee4e486568756fa028a04580546e21be
Thiago Oliveira <toliveirat@icloud.com>
2024-01-29 15:25:26 +0000

    another attempt to fix issue

--- main.tf
+++ main.tf
@@ -38,12 +38,12 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_groups
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
-  image_id      = data.aws_ami.app_ami.image_id
+  image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  }
+}
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
